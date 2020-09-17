1814f794c22e772fda97a00346a2114d2981d2f8
kwangyel <kwangyel@gmail.com>
2020-09-17 18:14:48 +0600

    test geoloc

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -39,30 +39,31 @@ export class LoginComponent implements OnInit {
 
   login() {
     this.submitted = true;
+    this.router.navigate(['selectzone']);
 
-    if (this.loginForm.valid) {
-      const loginId = this.loginForm.get('cid').value;
-      const password = this.loginForm.get('password').value;
-      this.authService.validateLogin(loginId, password).subscribe(response => {
-        sessionStorage.setItem('userId', response.data.id);
-        localStorage.setItem('loginId', loginId);
-        this.router.navigate(['selectzone']);
-        this.snackBar.open('Welcome Desuup ' + response.data.username, '', {
-          duration: 5000,
-          verticalPosition: 'bottom',
-          panelClass: ['success-snackbar']
-        });
-      },
-      error => {
-        this.submitted = false;
-        this.snackBar.open('Invalid login credentials, please try again', '', {
-          duration: 5000,
-          verticalPosition: 'bottom',
-          panelClass: ['error-snackbar']
-        });
-        console.log(error);
-      });
-    }
+    // if (this.loginForm.valid) {
+    //   const loginId = this.loginForm.get('cid').value;
+    //   const password = this.loginForm.get('password').value;
+    //   this.authService.validateLogin(loginId, password).subscribe(response => {
+    //     sessionStorage.setItem('userId', response.data.id);
+    //     localStorage.setItem('loginId', loginId);
+    //     this.router.navigate(['selectzone']);
+    //     this.snackBar.open('Welcome Desuup ' + response.data.username, '', {
+    //       duration: 5000,
+    //       verticalPosition: 'bottom',
+    //       panelClass: ['success-snackbar']
+    //     });
+    //   },
+    //   error => {
+    //     this.submitted = false;
+    //     this.snackBar.open('Invalid login credentials, please try again', '', {
+    //       duration: 5000,
+    //       verticalPosition: 'bottom',
+    //       panelClass: ['error-snackbar']
+    //     });
+    //     console.log(error);
+    //   });
+    // }
     // this.router.navigate(['selectzone']);
   }
 }
